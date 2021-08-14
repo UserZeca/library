89401c33079a5613a152126cb59820364160f33b
Ezequias Kluyvert de Oliveira Lemos <61207416+UserZeca@users.noreply.github.com>
2021-08-14 20:06:23 -0300

    Update db.js

--- backend-library/db.js
+++ backend-library/db.js
@@ -4,15 +4,3 @@ const config = require('./config');
 const db = firebase.initializeApp(config.firebaseConfig);
 
 module.exports = db;
-
-/*
-
-cloudinary.config({ 
-  cloud_name: 'dkgxwlji4', 
-  api_key: '964826622558483'
-
-production, 
-  api_secret: 'O54-3L7MIIaYxB1gY4c_M3DNZEw' 
-});
-
-*/
